da747b52392b220ed88dcb98a98a77f2f1925cbe
juniorworku <76052158+juniorworku@users.noreply.github.com>
2024-02-24 02:02:29 -0800

    updates

--- A BUDGET APP/A Budget App/src/App.jsx
+++ A BUDGET APP/A Budget App/src/App.jsx
@@ -1,21 +1,46 @@
+import { useState } from 'react'
 import { Button, Stack } from 'react-bootstrap'
 import Container from 'react-bootstrap/Container'
 import BudgetCard from './Components/BudgetCard'
+import AddBudgetModal from './Components/AddBudgetModal' 
+import { useBudgets } from './contexts/BudgetsContext'
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] =useState(false)
+  const { budgets } = useBudgets
 
-  return <Container className='my-4'>
-    <Stack direction='horizontal' gap="2" className='nb-4'>
-      <h1 className='me-auto'> Budgets</h1>
-      <Button variant="primary"> Add Budget</Button>
-      <Button variant="outline-primary"> Add Expense</Button>
-    </Stack>
-    <div style={{ display: "grid", gridTemplateColumns:"repeat(auto-fill, minmax(300px,1fr)", gap:"1rem", alignItems:"flex-start"}}>
-      
-      <BudgetCard name="Entertaiment" amount={200} max={1000} ></BudgetCard>
-    </div>
+  return (
+    <>
+    <Container className='my-4'>
+      <Stack direction='horizontal' gap="2" className='nb-4'>
+        <h1 className='me-auto'> Budgets</h1>
+        <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}> Add Budget</Button>
+        <Button variant="outline-primary"> Add Expense</Button>
+      </Stack>
+      <div style={{
+         display: "grid", 
+         gridTemplateColumns:"repeat(auto-fill, minmax(300px,1fr)", 
+         gap:"1rem", 
+         alignItems:"flex-start"}}>
 
-  </Container>
+        {budgets.map(budget => (
+          <BudgetCard 
+          key={budget.id}
+          name= {budget.name} 
+          amount={budget.amount} 
+          max={budget.max} />
+
+        ))}
+        
+        
+      </div>
+
+    </Container>
+    <AddBudgetModal show= {showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)} />
+    </>
+    
+
+  )
 }
 
 export default App
--- A BUDGET APP/A Budget App/src/Contexts/BudgetContext.js
+++
@@ -1,7 +0,0 @@
-export function useBudget(){
-
-}
-
-export const BudgetProvider =({ Children}) =>{
-    return Children
-}
